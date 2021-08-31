06da5af114c80d9c357f7901c222731c8f47f596
badal <badal1906@gmail.com>
2021-08-31 18:40:32 +0530

    state-mapped

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -1,27 +1,33 @@
-import "./preview.css"
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import "./preview.css";
 
 let Preview = () => {
-    return (
+  let { id } = useParams();
+  let state = useSelector((state) => state);
+  let reqObject = state[id];
+  return (
+    <>
       <div className="preview-container">
-      <div className="preview-img-container">
-        <img src="https://i.shgcdn.com/d28da852-3c05-408e-bde3-4aeb881e1a08/-/format/auto/-/preview/3000x3000/-/quality/lighter/" />
-      </div>
+        <div className="preview-img-container">
+          <img src={reqObject.img} />
+        </div>
 
-      <div className="preview-listing">
-        <h2>Title</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non iste
-          voluptas iusto earum reprehenderit sequi ratione quae numquam
-          ducimus ab assumenda ex, culpa, ipsam doloribus laborum sit vitae
-          delectus rem nam! Optio similique sunt, ut perferendis doloribus
-          nihil. Molestiae dolorem, voluptate eum vel deserunt commodi ut.
-          Ipsa ipsam cupiditate incidunt.
-        </p>
-        <button>Add to Cart</button>
+        <div className="preview-listing">
+          <h2>{reqObject.name}</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non iste
+            voluptas iusto earum reprehenderit sequi ratione quae numquam
+            ducimus ab assumenda ex, culpa, ipsam doloribus laborum sit vitae
+            delectus rem nam! Optio similique sunt, ut perferendis doloribus
+            nihil. Molestiae dolorem, voluptate eum vel deserunt commodi ut.
+            Ipsa ipsam cupiditate incidunt.
+          </p>
+          <button>Add to Cart</button>
+        </div>
       </div>
-    </div>
-    )
-    
-  };
-  
-  export default Preview;
+    </>
+  );
+};
+
+export default Preview;
