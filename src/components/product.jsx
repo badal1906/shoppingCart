f88bb1f89a7329fa03fbb4a55c23371e151edbc5
badal <badal1906@gmail.com>
2021-08-31 16:00:49 +0530

    ui completed

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -6,7 +6,6 @@ let Product = () => {
       <div className="product-img">
         <img
           src="https://i.shgcdn.com/d28da852-3c05-408e-bde3-4aeb881e1a08/-/format/auto/-/preview/3000x3000/-/quality/lighter/"
-          alt=""
         />
       </div>
       <div className="product-btn">
